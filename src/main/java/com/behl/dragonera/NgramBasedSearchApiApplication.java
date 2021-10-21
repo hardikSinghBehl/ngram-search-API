06dd0cf57a6a362d109bd07ae0301efccb687104
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-21 11:17:57 +0530

    init project

---
+++ src/main/java/com/behl/dragonera/NgramBasedSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.behl.dragonera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgramBasedSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgramBasedSearchApiApplication.class, args);
+	}
+
+}
